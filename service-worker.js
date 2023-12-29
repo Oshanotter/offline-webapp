fd26d5c4920d7262e1c700e3b3ad9266af64c6ac
Max Forst <103145365+Oshanotter@users.noreply.github.com>
2023-12-29 14:27:18 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -52,24 +52,22 @@ self.addEventListener('activate', event => {
 // The fetch handler serves responses for same-origin resources from a cache.
 // If no response is found, it populates the runtime cache with the response
 // from the network before returning it to the page.
-self.addEventListener('fetch', event => {
-  // Skip cross-origin requests, like those for Google Analytics.
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    // Try to respond with the content from the cache
+    caches.match(event.request).then((cachedResponse) => {
+      // If the network is available, fetch from the network in the background
+      const fetchPromise = fetch(event.request).then((networkResponse) => {
+        // Update the cache with the new response
+        caches.open(CACHE_PREFIX).then((cache) => {
+          cache.put(event.request, networkResponse.clone());
         });
+        return networkResponse;
       })
-    );
-  }
+      .catch(() => cachedResponse); // If network fetch fails, return the cached response
+
+      // Return the cached response immediately, but also fetch from the network in the background
+      return Promise.race([cachedResponse, fetchPromise]);
+    })
+  );
 });
