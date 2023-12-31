72be656f64b897ff4019d7075f6a2ece06632ddf
Max Forst <103145365+Oshanotter@users.noreply.github.com>
2023-12-31 17:52:32 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,261 @@
+var overlay;
+    var resultsPage;
+    var startText;
+    var gameList;
+    var listNum;
+    var allowColorChange = false;
+    var passCorrectList = [];
+    var cardList = [];
+    var currentPosition = "NEUTRAL";
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('service-worker.js');
-}
+    function shuffleList(array) {
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        // Swap array[i] and array[j]
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+    }
 
-document.querySelector('#show').addEventListener('click', () => {
-  const iconUrl = document.querySelector('select').selectedOptions[0].value;
-  let imgElement = document.createElement('img');
-  imgElement.src = iconUrl;
-  document.querySelector('#container').appendChild(imgElement);
-});
+    function createOverlay() {
+      overlay = document.createElement('div');
+      overlay.style.height = "100%";
+      overlay.style.width = "100%";
+      overlay.style.overflow = "hidden";
+      overlay.style.top = "0";
+      overlay.style.position = "fixed";
+
+      resultsPage = document.createElement('div');
+      resultsPage.style.height = "85%";
+      resultsPage.style.width = "90%";
+      resultsPage.style.top = "15%"
+      resultsPage.style.position = "fixed";
+      resultsPage.style.alignItems = "flex-start"
+      resultsPage.style.backgroundColor = "yellow";
+      overlay.appendChild(resultsPage);
+
+      var banner = document.createElement('div');
+      banner.style.backgroundColor = "blue";
+      banner.style.height = "15%";
+      banner.style.width = "100%";
+      banner.style.position = "absolute";
+      banner.style.top = "0";
+      overlay.appendChild(banner);
+
+      var circle = document.createElement('div');
+      circle.id = "circle"
+      circle.style.borderRadius = "50%";
+      circle.style.border =  "1vw solid blue";
+      circle.style.height = "10vw";
+      circle.style.width = "10vw";
+      circle.style.top = "80%"
+      circle.style.left = "50%";
+      circle.style.position = "absolute";
+      circle.style.transform = "translate(-50%, -50%)";
+      circle.innerText = '120';
+      circle.style.fontSize = "240%";
+      circle.style.fontWeight = "bold";
+      banner.appendChild(circle);
+
+      startText = document.createElement('div');
+      startText.innerText = "Get Ready\n5";
+      startText.style.position = "absolute";
+      startText.style.height = "80%";
+      startText.style.width = "80%";
+      startText.style.fontSize = "300%";
+      startText.style.backgroundColor = "rgba(255, 0, 0, 0)";
+      startText.style.top = "20%";
+      startText.style.textAlign = "center";
+      overlay.appendChild(startText);
+
+      const exitButton = document.createElement('div');
+      exitButton.style.top = "30%"
+      exitButton.style.left = "4%";
+      exitButton.style.position = "absolute";
+      exitButton.style.backgroundColor = "blue";
+      exitButton.style.fontSize = "150%";
+      exitButton.innerText = '< Back';
+      exitButton.addEventListener('click', function () {
+          document.body.removeChild(overlay);
+      });
+      banner.appendChild(exitButton);
+
+      document.body.appendChild(overlay);
+
+      getReady(6);
+    }
+
+    function getReady(countdown){
+        // start counting down to get ready
+        if (countdown == 1){
+            startGame();
+        }else{
+            var newNum = countdown - 1;
+            startText.innerText = "Get Ready\n" + newNum;
+            setTimeout(function(){
+                getReady(newNum);
+            }, 1000);
+        }
+    }
+
+    function startTimer(countdown){
+        // start the game timer
+        if (countdown == 1){
+            circle.innerText = "0";
+            passCorrectList.push("PASS");
+            cardList.push(gameList[listNum]);
+            endGame();
+        }else{
+            var newNum = countdown - 1;
+            circle.innerText = newNum;
+            setTimeout(function(){
+                startTimer(newNum)
+            }, 1000);
+        }
+    }
+
+    function startGame(){
+        // start the game timer
+        startTimer(10);
+        listNum = 0;
+        startText.innerText = gameList[listNum];
+        allowColorChange = true;
+    }
+
+    function getGameList(gameName){
+        // remove me later v
+        var list = ["Meowth", "Pikachu", "Clefairy"]
+        var gamesDict = {}
+        gamesDict["Gen I"] = list
+        // remove me later ^
+        // get the array from the dictionary
+        gameList = gamesDict[gameName];
+        shuffleList(gameList);
+        requestPermission();
+        createOverlay();
+    }
+
+    function handleOrientation(event) {
+        if (!allowColorChange){
+            if (typeof passCorrectList == 'undefined' || typeof cardList == 'undefined') {
+                passCorrectList = [];
+                cardList = [];
+            }
+            return;
+        }
+        if (event.gamma > -50 && event.gamma < 0) {
+            overlay.style.backgroundColor = 'red';
+            startText.innerText = "PASS"
+            if (currentPosition != "PASS"){
+                // add current item to passed list
+                passCorrectList.push("PASS")
+                cardList.push(gameList[listNum])
+                currentPosition = "PASS";
+            }
+        } else if (event.gamma < 60 && event.gamma > 0) {
+            overlay.style.backgroundColor = 'green';
+            startText.innerText = "CORRECT"
+            if (currentPosition != "CORRECT"){
+                // add current item to correct list
+                passCorrectList.push("CORRECT")
+                cardList.push(gameList[listNum])
+                currentPosition = "CORRECT";
+            }
+        } else {
+            overlay.style.backgroundColor = '';
+            if (currentPosition != "NEUTRAL"){
+                listNum += 1;
+                if (listNum > gameList.length - 1){
+                    listNum = 0;
+                }
+                startText.innerText = gameList[listNum];
+                currentPosition = "NEUTRAL";
+            }
+        }
+    }
+
+    function requestPermission() {
+        if (window.DeviceOrientationEvent) {
+            DeviceOrientationEvent.requestPermission()
+                .then(permissionState => {
+                    if (permissionState === 'granted') {
+                        window.addEventListener('deviceorientation', handleOrientation);
+                        return true;
+                    } else {
+                        return false;
+                    }
+                })
+                .catch(error => {
+                    console.error('Error requesting device orientation permission:', error);
+                    return false;
+                });
+        } else {
+            return false;
+        }
+    }
+
+    function endGame(){
+        // end the game and show the results
+        allowColorChange = false;
+        overlay.style.backgroundColor = ''
+        startText.innerText = "TIME'S UP!"
+        setTimeout(function(){
+            // display the results
+            startText.innerText = '';
+            var leftCol = document.createElement('div');
+            leftCol.style.width = "45%";
+            leftCol.style.height = "100%";
+            leftCol.style.justifyContent = "start";
+            leftCol.style.flexDirection = "column";
+            leftCol.style.display = "flex";
+            leftCol.style.fontSize = "150%";
+            leftCol.style.backgroundColor = "orange";
+            leftCol.style.height = "auto";
+            resultsPage.appendChild(leftCol)
+
+            var middleCol = document.createElement('div');
+            middleCol.style.width = "10%";
+            middleCol.style.backgroundColor = "purple";
+            resultsPage.appendChild(middleCol);
+
+            var rightCol = document.createElement('div');
+            rightCol.style.width = "45%";
+            rightCol.style.height = "100%";
+            rightCol.style.justifyContent = "start";
+            rightCol.style.flexDirection = "column";
+            rightCol.style.display = "flex";
+            rightCol.style.fontSize = "150%";
+            rightCol.style.backgroundColor = "pink";
+            rightCol.style.height = "auto";
+            resultsPage.appendChild(rightCol)
+
+            resultsPage.style.overflowY = "auto";
+            overlay.removeChild(startText);
+
+            for (var i = 0; i < passCorrectList.length; i++){
+                var card = cardList[i];
+                if (passCorrectList[i] == "CORRECT"){
+                    var color = "green";
+                }else{
+                    var color = "red";
+                }
+                var text = document.createElement('span');
+                text.innerText = card;
+                text.style.color = color;
+                var evenOdd = i % 2;
+                if (evenOdd == 0){
+                    // it is even, add to left list
+                    leftCol.appendChild(text)
+                }else{
+                    // it is odd, add to right list
+                    rightCol.appendChild(text)
+                }
+            }
+            /*
+            if (cardList.length % 2 == 1){
+                // add another card to the right column to make them even
+                var text = document.createElement('span');
+                text.innerText = "hi there, this is some very, extra long text";
+                rightCol.appendChild(text);
+            }*/
+        }, 3000);
+    }
