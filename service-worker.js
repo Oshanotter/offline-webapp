065c07450cf01ac7ca8d27cf5e367f33e4b6e6d2
Max Forst <103145365+Oshanotter@users.noreply.github.com>
2024-01-01 17:40:16 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,4 @@
-/*
- Copyright 2016 Google Inc. All Rights Reserved.
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
- http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- */
-
-// Names of the two caches used in this version of the service worker.
-// Change to v2, etc. when you update any of the local resources, which will
-// in turn trigger the install event again.
-const PRECACHE = 'precache-v2';
+const PRECACHE = 'precache';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
@@ -55,7 +39,7 @@ self.addEventListener('activate', event => {
 // from the network before returning it to the page.
 // On fetch, use cache but update the entry with the latest contents from the server.
 self.addEventListener('fetch', function(evt) {
-  console.log('The service worker is serving the asset.');
+  console.log('page loading');
   // You can use respondWith() to answer ASAP…
   evt.respondWith(fromCache(evt.request));
   // ...and waitUntil() to prevent the worker to be killed until the cache is updated.
@@ -68,14 +52,37 @@ self.addEventListener('fetch', function(evt) {
 
 // Open the cache where the assets were stored and search for the requested resource. Notice that in case of no matching, the promise still resolves but it does with undefined as value.
 function fromCache(request) {
-  return caches.open(RUNTIME).then(function (cache) {
-    return cache.match(request);
-  });
+  console.log("Fetching from cache");
+
+  return caches.open(RUNTIME)
+    .then((cache) => {
+      return cache.match(request);
+    })
+    .catch((error) => {
+      console.error("Runtime cache error:", error);
+      return runDefault(); // Run the default function if runtime cache fails
+    })
+    .then((response) => {
+      // If response is not found in runtime cache, try precache
+      if (!response) {
+        return caches.open(PRECACHE).then((cache) => {
+          return cache.match(request);
+        });
+      } else {
+        return response;
+      }
+    })
+    .catch((error) => {
+      console.error("Precache cache error:", error);
+      throw error; // Re-throw the error for further handling if needed
+    });
 }
+
 
 // Update consists in opening the cache, performing a network request and storing the new response data.
 function update(request) {
-  return caches.open(RUNTIME).then(function (cache) {
+ console.log("updating cache")
+  return caches.open(PRECACHE).then(function (cache) {
     return fetch(request).then(function (response) {
       return cache.put(request, response.clone()).then(function () {
         return response;
